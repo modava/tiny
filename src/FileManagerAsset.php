696c0eeda5d5197c99caa8373a7de6768df46b99
Mong Dao <mongdaovan86@yahoo.com>
2020-05-03 02:12:29 +0700

    start widgets tiny

---
+++ src/FileManagerAsset.php
@@ -0,0 +1,12 @@
+<?php
+namespace modava\tiny;
+
+use yii\web\AssetBundle;
+
+class FileManagerAsset extends AssetBundle
+{
+    public $sourcePath = '@backend/widgets/tiny/src/assets';
+
+    public $js = [
+    ];
+}
---
+++ src/TinyMceAsset.php
@@ -0,0 +1,13 @@
+<?php
+namespace modava\tiny;
+
+use yii\web\AssetBundle;
+
+class TinyMceAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/tinymce/tinymce';
+
+    public $js = [
+        'tinymce.js'
+    ];
+}
